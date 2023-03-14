43796a49e220b3fe7c0cfecbf54780dcf3ec9505
chochojj <csgy114@gmail.com>
2023-03-14 16:31:24 +0900

    Create header.js

---
+++ todolist/src/components/header.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Mainheader = styled.header`
+  
+`
+
+const Header = () => {
+ 
+
+  return (
+    <Mainheader>
+
+    </Mainheader>
+  );
+};
+
+export default Header;
