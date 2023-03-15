91867a4c00e6b420022fa50078f90d0b5a2e04e9
chochojj <csgy114@gmail.com>
2023-03-16 01:24:13 +0900

    사이드 바 탭 리스트 진행중

--- todolist/src/components/SideBar.js
+++ todolist/src/components/SideBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-const Side = styled.div`
+const Side = styled.nav`
   &{position: fixed;
   top: 70px;
   right: 0;
@@ -9,7 +9,13 @@ const Side = styled.div`
   height: 100vh;
   background-color: rgba(255,255,255,0.7);}
   box-shadow: 0px 4px 4px rgba(0,0,0, 0.25);
+
+  
 `
+const StyledLink  = styled(Link)`
+`
+
+
 //애니메이션으로 부드러운 효과를 주려면
 //클릭 이벤트를 했을때 위치 이동을 시켜주는 css를 이벤트로 넣어야 할듯
 //예시
@@ -28,12 +34,10 @@ const Side = styled.div`
 function SideBar() {
   return (
     <Side>
-        <nav>
-            <Link to="/">할 일 보기</Link>
-            <Link to="/diary">내 일기장</Link>
-            <Link to="/calendar">달력 보기</Link>
-            <Link to="/madeby">만든 사람</Link>
-        </nav>
+        <StyledLink to="/">할 일 보기</StyledLink>
+        <StyledLink to="/diary">내 일기장</StyledLink>
+        <StyledLink to="/calendar">달력 보기</StyledLink>
+        <StyledLink to="/madeby">만든 사람</StyledLink>
     </Side>
   );
 }
