5a21ff87f159febf1599f6c6227610aa2236c5f6
chochojj <csgy114@gmail.com>
2023-03-22 23:51:23 +0900

    일기장 기능

--- todolist/src/pages/Diary.js
+++ todolist/src/pages/Diary.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const todaylog = styled.div`
@@ -6,9 +6,39 @@ const todaylog = styled.div`
 `
 
 function Diary() {
+  const [diaries, setDiaries] = useState([]);
+  const [inputTitle, setInputTitle] = useState('');
+  const [inputContent, setInputContent] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const currentDate = new Date().toLocaleDateString();
+    setDiaries([...diaries, { title: inputTitle, content: inputContent, date: currentDate }]);
+    setInputTitle('');
+    setInputContent('');
+  };
+
+  const handleDelete = (index) => {
+    const newDiaries = [...diaries];
+    newDiaries.splice(index, 1);
+    setDiaries(newDiaries);
+  };
+
   return (
     <todaylog>
-
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="제목" value={inputTitle} onChange={(e) => setInputTitle(e.target.value)} />
+        <textarea placeholder="내용" value={inputContent} onChange={(e) => setInputContent(e.target.value)} />
+        <button type="submit">작성</button>
+      </form>
+      <ul>
+        {diaries.map((diary, index) => (
+          <li key={index}>
+            {diary.title} ({diary.date})
+            <button onClick={() => handleDelete(index)}>삭제</button>
+          </li>
+        ))}
+      </ul>
     </todaylog>
   );
 }
