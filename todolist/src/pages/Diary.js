0c54f862bbc740dc9abf297c05070f2fa76e7c1e
chochojj <csgy114@gmail.com>
2023-03-23 00:00:00 +0900

    빈칸이면 경고 뜨는 기능

--- todolist/src/pages/Diary.js
+++ todolist/src/pages/Diary.js
@@ -9,16 +9,19 @@ function Diary() {
   const [diaries, setDiaries] = useState([]);
   const [inputTitle, setInputTitle] = useState('');
   const [inputContent, setInputContent] = useState('');
+  const [emptyInput, setEmptyInput] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!inputTitle || !inputContent) {
+      setEmptyInput(true);
       return;
     }
     const currentDate = new Date().toLocaleDateString();
     setDiaries([...diaries, { title: inputTitle, content: inputContent, date: currentDate }]);
     setInputTitle('');
     setInputContent('');
+    setEmptyInput(false);
   };
 
   const handleDelete = (index) => {
@@ -36,11 +39,12 @@ function Diary() {
           value={inputTitle} 
           onChange={(e) => setInputTitle(e.target.value)} 
         />
-        <textarea 
-          placeholder="내용" 
-          value={inputContent} 
-          onChange={(e) => setInputContent(e.target.value)} 
+        <textarea
+          placeholder="내용"
+          value={inputContent}
+          onChange={(e) => setInputContent(e.target.value)}
         />
+        {emptyInput && <div>입력칸이 비었습니다.</div>}
         <button type="submit">작성</button>
       </form>
       <ul>
