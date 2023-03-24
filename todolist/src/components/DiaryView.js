97e8402bd3efbde36f618610f7a875e683a23428
chochojj <csgy114@gmail.com>
2023-03-24 17:47:53 +0900

    스타일 컴포넌트 시작

--- todolist/src/components/DiaryView.js
+++ todolist/src/components/DiaryView.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import styled from 'styled-components';
 
 function DiaryView({ title, content, date, setSelectedDiaryIndex, diaries, setDiaries, selectedDiaryIndex }) {
   const [editableTitle, setEditableTitle] = useState(title);
