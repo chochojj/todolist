4983399a41e88c31ddd4591e085e352f2ad1daa1
chochojj <csgy114@gmail.com>
2023-04-07 13:36:42 +0900

    calender 렌더링 시켜주기

---
+++ src/pages/Calendar.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Cal = styled.div`
+    
+`
+
+function Calendar() {
+  return (
+    <Cal>
+      암것두 없어용
+    </Cal>
+  );
+}
+
+export default Calendar;
